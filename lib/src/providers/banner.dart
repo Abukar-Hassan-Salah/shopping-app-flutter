d23cb92b6ea9d0592b9604d34f52f32faddb35c3
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2024-02-01 18:00:52 +0300

    first commit

---
+++ lib/src/providers/banner.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:ecommerce_app/src/constants.dart';
+import 'package:ecommerce_app/src/models/banner.dart';
+import 'package:http/http.dart' as http;
+
+class BannerProvider {
+  getBanners() async {
+    var response = await http.get(Uri.parse("${kEndPoint}banners"));
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+      if (!decodedData["status"]) throw decodedData["data"];
+      List data = decodedData["data"];
+      // ignore: avoid_print
+      print(data);
+      return data.map((e) => BannerModel.fromJson(e)).toList();
+    }
+  }
+}
---
+++ lib/src/views/screens/profile.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: const [
+          Center(child: Text("Profile"))
+        ],
+      ),
+    );
+  }
+}
